0df9f6054fe8ff1b4df91e42c15947eedab93d34
Albreis - Design & Programação <contato@albreis.com.br>
2022-12-12 08:08:02 -0300

    change public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   outputDir: './docs',
-  publicPath: '/docs',
+  publicPath: '/menu-lol/',
   transpileDependencies: true
 })
